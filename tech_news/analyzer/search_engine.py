3c713542d8560d8a07910300aba09015dc72e5e0
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-16 18:10:35 -0300

    feat: realizando busca por data no banco

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -12,14 +12,16 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    if not date or datetime.fromisoformat(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
         raise ValueError("Data inválida")
-    date_object = datetime.strptime(date, "%Y-%m-%d")
-    date = date_object.strftime("%d/%m/%Y")
-    news = search_news({"timestamp": {"$regex": date, "$options": "i"}})
-    if not news:
-        return []
-    return [(new['title'], new['url']) for new in news]
+    else:
+        date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        news = search_news({"timestamp": {"$regex": date, "$options": "i"}})
+        if not news:
+            return []
+        return [(new['title'], new['url']) for new in news]
 
 
 # Requisito 9
