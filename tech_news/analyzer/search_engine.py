ae15d31fd94e92e3cf9d54fa3f2822f064e3aa7b
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-16 17:51:27 -0300

    feat: realizando busca por titulo no banco

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    if not news:
+        return []
+    return [(new['title'], new['url']) for new in news]
 
 
 # Requisito 8
