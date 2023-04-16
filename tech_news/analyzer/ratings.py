7207f151cb0bd0f40bba47391171e2a314edbdfa
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-04-16 18:41:18 -0300

    feat: realizando busca pelas top five categorias

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = search_news({"category": {"$exists": True}})
+    if not news:
+        return []
+    categories = [new['category'] for new in news]
+    print(categories)
+    top_five_categories = sorted(
+        set(categories), key=lambda cat: (-categories.count(cat), cat))[:5]
+    print(top_five_categories)
+    return top_five_categories
